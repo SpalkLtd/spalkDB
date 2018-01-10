296717976b502f00175226a720a3322273d2ab55
Dion Bramley <dionbramley@gmail.com>
2018-01-10 17:57:19 +1300

    refactor to be slightly more elegant. Muahahaha

--- spalkDB.go
+++ spalkDB.go
@@ -3,6 +3,7 @@ package spalkDB
 
 import (
 	"bytes"
+	"database/sql"
 	"errors"
 	"fmt"
 	"reflect"
@@ -13,17 +14,17 @@ import (
 
 // the first parameter must satisfy Builder, or be a dbr.InsertBuilder
 // The last parameter must be a struct
-func MapStruct(b interface{}, cols []string, value interface{}) error {
+func MapStruct(b interface{}, cols []string, value interface{}) func() (sql.Result, error) {
 	switch b.(type) {
 	case *dbr.UpdateBuilder:
 	case *dbr.InsertBuilder:
 	default:
-		return errors.New("First parameter to MapStruct must be a Builder, dbr.UpdateBuilder, or dbr.InsertBuilder")
+		panic(errors.New("First parameter to MapStruct must be a Builder, dbr.UpdateBuilder, or dbr.InsertBuilder"))
 	}
 
 	rt := reflect.TypeOf(value)
 	if rt.Kind() != reflect.Struct {
-		return errors.New("unsupported type passed in as value. value must be a struct.")
+		panic(errors.New("unsupported type passed in as value. value must be a struct."))
 	}
 	count := rt.NumField()
 	fields := make([]reflect.StructField, count)
@@ -73,10 +74,16 @@ colLoop:
 					continue colLoop
 				}
 			}
-			return errors.New(fmt.Sprintf("no match found for column %s in struct", c))
+			panic(errors.New(fmt.Sprintf("no match found for column %s in struct", c)))
 		}
 	}
 
+	switch v := b.(type) {
+	case *dbr.UpdateBuilder:
+		return v.Exec
+	case *dbr.InsertBuilder:
+		return v.Exec
+	}
 	return nil
 }
 
